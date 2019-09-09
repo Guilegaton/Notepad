35b83d1f14ac0db1f994bfaa1170be36f0332e59
Julii Vedkal <julii.vedkal@ssa.group>
2019-09-09 19:12:19 +0300

    [add] - default file entity

---
+++ Notepad/Models/File.cs
@@ -0,0 +1,14 @@
+﻿using Notepad.SharedKernel.Models;
+
+namespace Notepad.Models
+{
+    public class File : BaseEntity
+    {
+        #region Public Properties
+
+        public byte[] Data { get; set; }
+        public string Name { get; set; }
+
+        #endregion Public Properties
+    }
+}
---
+++ Notepad/SharedKernel/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Notepad.SharedKernel.Models
+{
+    public class BaseEntity
+    {
+        #region Public Properties
+
+        public long Id { get; set; }
+
+        #endregion Public Properties
+    }
+}
